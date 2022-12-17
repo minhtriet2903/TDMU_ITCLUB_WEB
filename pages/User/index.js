383fbf2e8b621211421ec9b473d4572ebc6a0032
DESKTOP-5338IE1\thang <thangcdnguyen@gmail.com>
2022-12-17 21:31:42 +0700

    Page Profile, logo mạng xã hội

--- pages/User/index.js
+++ pages/User/index.js
@@ -12,9 +12,18 @@ import {
   Checkbox,
   Upload,
   Timeline,
+  Divider,
 } from "antd";
 import React, { useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
+import {
+  faAlignCenter,
+  faBrain,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faMailBulk, faFax } from "@fortawesome/free-solid-svg-icons";
+import Nav from "../../src/components/NavBar.js";
 
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
@@ -26,116 +35,265 @@ const App = () => {
     setComponentDisabled(disabled);
   };
   return (
-    <div className="mt-10 flex justify-evenly">
-      <div className="flex flex-col">
-        <div className="bg-slate-500 rounded-full w-[50px] h-[50px]"></div>
-        <div>
-          <Form
-            labelCol={{
-              span: 4,
-            }}
-            wrapperCol={{
-              span: 14,
-            }}
-            layout="horizontal"
-            onValuesChange={onFormLayoutChange}
-            disabled={componentDisabled}
+    <div>
+      <Nav />
+      <div className="container flex-wrap items-center justify-between mx-auto mt-3 mb-3">
+        <div className="main-body">
+          <nav
+            class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+            aria-label="Breadcrumb"
           >
-            <Form.Item label="Chekbox" name="disabled" valuePropName="checked">
-              <Checkbox>Checkbox</Checkbox>
-            </Form.Item>
-            <Form.Item label="Radio">
-              <Radio.Group>
-                <Radio value="apple"> Apple </Radio>
-                <Radio value="pear"> Pear </Radio>
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item label="Input">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Select">
-              <Select>
-                <Select.Option value="demo">Demo</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item label="TreeSelect">
-              <TreeSelect
-                treeData={[
-                  {
-                    title: "Light",
-                    value: "light",
-                    children: [
-                      {
-                        title: "Bamboo",
-                        value: "bamboo",
-                      },
-                    ],
-                  },
-                ]}
-              />
-            </Form.Item>
-            <Form.Item label="Cascader">
-              <Cascader
-                options={[
-                  {
-                    value: "zhejiang",
-                    label: "Zhejiang",
-                    children: [
-                      {
-                        value: "hangzhou",
-                        label: "Hangzhou",
-                      },
-                    ],
-                  },
-                ]}
-              />
-            </Form.Item>
-            <Form.Item label="DatePicker">
-              <DatePicker />
-            </Form.Item>
-            <Form.Item label="RangePicker">
-              <RangePicker />
-            </Form.Item>
-            <Form.Item label="InputNumber">
-              <InputNumber />
-            </Form.Item>
-            <Form.Item label="TextArea">
-              <TextArea rows={4} />
-            </Form.Item>
-            <Form.Item label="Switch" valuePropName="checked">
-              <Switch />
-            </Form.Item>
-            <Form.Item label="Upload" valuePropName="fileList">
-              <Upload action="/upload.do" listType="picture-card">
-                <div>
-                  <PlusOutlined />
-                  <div
-                    style={{
-                      marginTop: 8,
-                    }}
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+              <li class="inline-flex items-center">
+                <a
+                  href="#"
+                  class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                >
+                  <svg
+                    class="w-4 h-4 mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Upload
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                  </svg>
+                  Home
+                </a>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg
+                    class="w-6 h-6 text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                    Profile
+                  </span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+          <div class="grid mt-5 grid-cols-3">
+            <div class="profile-left w-full">
+              <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 pt-5">
+                <div class="flex flex-col items-center pb-10">
+                  <img
+                    class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src="/googleLogo.png"
+                    alt="PersonImage"
+                  />
+                  <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                    Nguyễn Phạm Minh Triết
+                  </h5>
+                  <span class="text-sm text-gray-500 dark:text-gray-400">
+                    Vua của mọi ngành
+                  </span>
+                  <div class="flex mt-4 space-x-3 md:mt-6">
+                    <a
+                      href="#"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Follow
+                    </a>
+                    <a
+                      href="#"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
+                    >
+                      Message
+                    </a>
                   </div>
                 </div>
-              </Upload>
-            </Form.Item>
-            <Form.Item label="Button">
-              <Button>Button</Button>
-            </Form.Item>
-          </Form>
+              </div>
+
+              <div class="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-5">
+                <div class="flow-root">
+                  <ul
+                    role="list"
+                    class="divide-y divide-gray-200 dark:divide-gray-700"
+                  >
+                    <li class="py-3 sm:py-4">
+                      <div class="flex items-center space-x-4">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="w-8 h-8 rounded-full"
+                            src="/github-mark-white.png"
+                            alt="GithubLogo"
+                          ></img>
+                        </div>
+                        <div class="flex-1 min-w-0">
+                          <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Github
+                          </p>
+                        </div>
+                        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                          //Link
+                        </div>
+                      </div>
+                    </li>
+                    <li class="py-3 sm:py-4">
+                      <div class="flex items-center space-x-4">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="w-8 h-8 rounded-full bg-white"
+                            src="/twitter.png"
+                            alt="twitterLogo"
+                          ></img>
+                        </div>
+                        <div class="flex-1 min-w-0">
+                          <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Twitter
+                          </p>
+                        </div>
+                        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                          //Link
+                        </div>
+                      </div>
+                    </li>
+                    <li class="py-3 sm:py-4">
+                      <div class="flex items-center space-x-4">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="w-8 h-8 rounded-full bg-white"
+                            src="/instagram.png"
+                            alt="InstagramLogo"
+                          ></img>
+                        </div>
+                        <div class="flex-1 min-w-0">
+                          <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Instagram
+                          </p>
+                        </div>
+                        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                          //Link
+                        </div>
+                      </div>
+                    </li>
+                    <li class="pt-3 pb-0 sm:pt-4">
+                      <div class="flex items-center space-x-4">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="w-8 h-8 rounded-full bg-white"
+                            src="/facebook.png"
+                            alt="FacebookLogo"
+                          ></img>
+                        </div>
+                        <div class="flex-1 min-w-0">
+                          <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Facebook
+                          </p>
+                        </div>
+                        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                          //Link
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="profile-right w-full col-span-2">
+              <form class="w-full">
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-first-name"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                      id="grid-first-name"
+                      type="text"
+                      placeholder="Jane"
+                    ></input>
+                  </div>
+                  <div class="w-full md:w-1/2 px-3">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-last-name"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-last-name"
+                      type="text"
+                      placeholder="Doe"
+                    ></input>
+                  </div>
+                </div>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full px-3">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-mssv"
+                    >
+                      Student Code
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-mssv"
+                      type="text"
+                      placeholder=" "
+                    ></input>
+                  </div>
+                </div>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full px-3">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-email"
+                    >
+                      Email address
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-email"
+                      type="email"
+                      placeholder=" "
+                    ></input>
+                  </div>
+                </div>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full px-3">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-phone"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-phone"
+                      type="text"
+                      placeholder=" "
+                    ></input>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <Timeline mode={mode}>
-          <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Solve initial network problems
-          </Timeline.Item>
-          <Timeline.Item>Technical testing</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-        </Timeline>
+      <div className="h-[50px] bg-sky-200 flex justify-center items-center font-bold">
+        <div>Phát triển bởi Câu lạc bộ IT TDMU</div>
+        <div>
+          <FontAwesomeIcon
+            icon={faMailBulk}
+            className="w-[20px] h-[20px] mx-2"
+          />
+          <FontAwesomeIcon icon={faPhone} className="w-[20px] h-[20px] mx-2" />
+          <FontAwesomeIcon icon={faFax} className="w-[20px] h-[20px] mx-2" />
+        </div>
       </div>
     </div>
   );
